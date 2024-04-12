d097c6bd38e61fcc133c3f8cc17ac9eea3f81ee9
Mohamed Aseef M <134759368+Aseefmohamed@users.noreply.github.com>
2024-04-12 18:54:52 +0530

    Update aseef.js

--- aseef.js
+++ aseef.js
@@ -1,10 +1,10 @@
 const accountSid = 'ACda4b698b17d826a87973bc90821240c9';
-const authToken = '65227ded3a0fbf7d50b39557934772aa';
+const authToken = '6c6ff41193b1633cd6e4147c712c901b';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-    body: 'Hello Aseef! ',
+    body: 'Warning your frd croborder!',
     from: '+12513698431',
     to: '+918015868478'
   })
